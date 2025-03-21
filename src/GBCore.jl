3ce7b0959dcd414ebbc6b99a5e10b5ab7d00063c
jeffersonfparil <jeffersonparil@gmail.com>
2025-03-21 13:57:58 +1100

    restructuring + testing imputef implementation

--- src/GBCore.jl
+++ src/GBCore.jl
@@ -15,26 +15,26 @@ using Lux # for Fit deep learning model
 
 # Structs
 include("all_structs.jl")
-# Genomes
+# Genomic data
 include("genomes/genomes.jl")
 include("genomes/filter.jl")
 include("genomes/merge.jl")
 include("genomes/impute.jl")
-# Phenomes
+# Phenomic data
 include("phenomes/phenomes.jl")
 include("phenomes/filter.jl")
 include("phenomes/merge.jl")
-# Trials
+# Trials data: single- or multi- environments, years, seasons and harvests
 include("trials/trials.jl")
 include("trials/filter.jl")
-# Trial-estimated breeding values
+# Trial-estimated breeding values: extraction of BLUPs of entries
 include("tebv/tebv.jl")
 include("tebv/linear_mixed_modelling.jl")
-# Model fit struct
+# Linear and non-linear genotype-to-phenotype models struct
 include("fit/fit.jl")
-# Model cross-validation struct
+# Genotype-to-phenotype model cross-validation struct
 include("cv/cv.jl")
-# Simulations
+# Simulations: genomes, trials, and mating
 include("simulation/simulate_effects.jl")
 include("simulation/simulate_genomes.jl")
 include("simulation/simulate_trials.jl")
