85109e9dd212ed7011f31920e61f527d8c779860
paril <Jeff Paril>
2025-03-29 16:03:03 +1100

    working on blr

--- src/tebv/bayesian_modelling.jl
+++ src/tebv/bayesian_modelling.jl
@@ -5,9 +5,9 @@ function analyse(
     other_covariates::Union{String,Nothing} = nothing,
     verbose::Bool = false,
 )::TEBV
-    # genomes = simulategenomes(n=500, l=10_000)
-    # trials, simulated_effects = simulatetrials(genomes = genomes, n_years=10, n_seasons=2, n_harvests=1, n_sites=2, n_replications=3)
-    # GRM::Union{Matrix{Float64}, UniformScaling} = I; traits = ["trait_1"]; other_covariates::Union{String, Nothing} = nothing; verbose::Bool = true'
+    # genomes = simulategenomes(n=500, l=1_000)
+    # trials, simulated_effects = simulatetrials(genomes = genomes, n_years=1, n_seasons=2, n_harvests=1, n_sites=2, n_replications=3)
+    # GRM::Union{Matrix{Float64}, UniformScaling} = I; traits = ["trait_1"]; other_covariates::Union{String, Nothing} = nothing; verbose::Bool = true;
     # Check arguments
     if !checkdims(trials)
         error("The Trials struct is corrupted.")
@@ -28,18 +28,37 @@ function analyse(
             )
         end
     end
-    # Tabularise the trials data
-    df = tabularise(trials)
-    # Make sure the blocks, rows, and columns are specific to each site
-    df.blocks = string.(df.sites, "|", df.blocks)
-    df.rows = string.(df.sites, "|", df.rows)
-    df.cols = string.(df.sites, "|", df.cols)
+    # Extract the entries which we want the estimated breeding values for
+    entries = sort(unique(trials.entries))
+    n = length(entries)
     # Define the traits
     traits = if length(traits) == 0
         trials.traits
     else
         traits
     end
+    # Instantiate output
+    tebv = TEBV(
+        traits = traits,
+        formulae = string.(traits, " ~ 1"),
+        models = ([""], rand(1), rand(1, 1)),
+        df_BLUEs = fill(DataFrame(), length(traits)),
+        df_BLUPs = fill(DataFrame(), length(traits)),
+        phenomes = [
+            begin
+                p = Phenomes(n = n, t = 1)
+                p.entries = entries
+                p.traits = [t]
+                p
+            end for t in traits
+        ],
+    )
+    # Tabularise the trials data
+    df = tabularise(trials)
+    # Make sure the blocks, rows, and columns are specific to each site
+    df.blocks = string.(df.sites, "|", df.blocks)
+    df.rows = string.(df.sites, "|", df.rows)
+    df.cols = string.(df.sites, "|", df.cols)
     # Identify non-fixed factors
     factors_all::Vector{String} = ["years", "seasons", "harvests", "sites", "blocks", "rows", "cols", "entries"]
     factors::Vector{String} = []
@@ -53,12 +72,21 @@ function analyse(
     if ("blocks" ∈ factors) && ("rows" ∈ factors)
         factors = filter(x -> x != "blocks", factors)
     end
+    # Check for potential out-of-memory error
+    D = dimensions(trials)
+    total_parameters = 1
+    for f in factors
+        # f = factors[1]
+        total_parameters *= (D[string("n_", f)] - 1)
+    end
+    total_X_size_in_Gb = nrow(df) * (total_parameters + 1) * sizeof(Float64) / (1024^3)
     # Iterate per trait
     for trait in traits
         # trait = traits[1]
         # Define the formula for the model
         formula_string = string(trait, " ~ 1 + ", join(factors, " * "))
         formula_full = @eval(@string2formula($formula_string))
+        X = modelmatrix(formula_full, df)
 
 
 
@@ -66,6 +94,31 @@ function analyse(
         _, col_labels = coefnames(apply_schema(f0, schema(f0, df)))
         size(df)
         length(col_labels)
-        X = modelmatrix(formula, df)
     end
+end
+
+Turing.@model function turing_bayesG(G, y)
+    # Set variance prior.
+    σ² ~ Distributions.Exponential(1.0 / std(y))
+    # σ² ~ truncated(Normal(init["σ²"], 1.0); lower=0)
+    # Set intercept prior.
+    intercept ~ Turing.Flat()
+    # intercept ~ Distributions.Normal(init["b0"], 1.0)
+    # Set the priors on our coefficients.
+    # p = size(G, 2)
+    coefficients ~ Distributions.MvNormal(zeros(size(G, 2)), I)
+    # Calculate all the mu terms.
+    mu = intercept .+ G * coefficients
+    # Return the distrbution of the response variable, from which the likelihood will be derived
+    return y ~ Distributions.MvNormal(mu, σ² * I)
+end
+
+function mcmc()
+    rng::TaskLocalRNG = Random.seed!(seed)
+    model = turing_model(X, y)
+    sampling_function =
+        NUTS(n_burnin, 0.65, max_depth = 5, Δ_max = 1000.0, init_ϵ = 0.2; adtype = AutoReverseDiff(compile = true))
+    chain = Turing.sample(rng, model, sampling_function, n_iter - n_burnin, progress = verbose)
+    # Use the mean paramter values after 150 burn-in iterations
+    params = Turing.get_params(chain[(n_burnin+1):end, :, :])
 end
