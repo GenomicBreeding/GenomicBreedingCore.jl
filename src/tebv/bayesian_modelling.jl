5e810fa7bd9a47448ab774f94d4f38ecc9882ab8
Jeff Paril <55180317+jeffersonfparil@users.noreply.github.com>
2025-05-01 19:15:23 +1000

    Update bayesian_modelling.jl

--- src/tebv/bayesian_modelling.jl
+++ src/tebv/bayesian_modelling.jl
@@ -270,7 +270,7 @@ julia> blr_2 = instantiateblr(trait = trials.traits[1], factors = ["years", "sea
 julia> length(blr_1.Xs) == 4
 true
 
-julia> size(blr_1.Xs["rows"]) == (15, 5)
+julia> size(blr_1.Xs["rows"]) == (15, 3)
 true
 
 julia> length(blr_2.Xs) == 9
